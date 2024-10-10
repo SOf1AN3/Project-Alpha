d1c5c3476e9b90a4adcb7175047b2f043138c263
Sofiane ASMA <sofiane.asma@proton.me>
2024-10-10 23:25:30 +0100

    Trying to fix build css error

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import AboutUs from '../components/AboutUs'
+import '../styles/about.css'
 
 const AboutPage = () => {
      return (
