97f246c24733464c807f37d3931604db3c253af6
SOf1AN3 <sofiane.asma@proton.me>
2024-11-24 13:19:44 +0100

    Basic messaging system functioal

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const mongoose = require("mongoose");
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+     cors: {
+          origin: 'http://localhost:5173',
+          methods: ['GET', 'POST'],
+          allowedHeaders: ['Content-Type', 'Authorization'],
+          credentials: true,
+     }
+});
 
 // Configuration de CORS
 app.use(cors({
@@ -25,10 +36,23 @@ app.use('/auth', authRoutes);
 mongoose.connect(process.env.MONGO_URI)
      .then(() => {
           console.log("Connected to MongoDB");
-          app.listen(process.env.PORT, () => {
+          server.listen(process.env.PORT, () => {
                console.log("Server listening on port", process.env.PORT);
           });
      })
      .catch((error) => {
           console.log("MongoDB connection error:", error);
      });
+
+// Gestion des événements Socket.io
+io.on('connection', (socket) => {
+     console.log('New client connected');
+
+     socket.on('sendMessage', (message) => {
+          io.emit('receiveMessage', message);
+     });
+
+     socket.on('disconnect', () => {
+          console.log('Client disconnected');
+     });
+});
