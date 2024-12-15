823666296cecd72bfa8520a8cbc743d2eeb543e0
SOf1AN3 <sofiane.asma@proton.me>
2024-12-15 19:06:38 +0100

    Fixed a linking bug

--- frontend/src/components/ProfileSettings.jsx
+++ frontend/src/components/ProfileSettings.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { useTranslation } from 'react-i18next';
 import i18n from '../i18n';
-import '../styles/ProfileSettings.css';
+import '../styles/profileSettings.css';
 
 const ProfileSettings = () => {
    const { t } = useTranslation();
